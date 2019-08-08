e02d90f7ba74ed622c28513c7040014b4f0c4dd6
Tim Kilian <tim.kilian@edel-optics.de>
2019-08-08 13:31:31 +0200

    Initialize eslint config

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+    rules: {
+        'no-console': 0,
+        'indent': [ 2, 4, { SwitchCase: 1 } ],
+        'no-unused-vars': 1,
+        'space-before-function-paren': [ 2, 'always' ],
+        'object-curly-spacing': [ 2, 'always' ],
+        'array-bracket-spacing': [ 2, 'always' ],
+        'require-jsdoc': [ 0, {
+            'require': {
+                'FunctionDeclaration': true,
+                'MethodDefinition': true,
+                'ClassDeclaration': true
+            }
+        } ],
+        'padded-blocks': [ 2, { classes: 'always' } ]
+    }
+};
