7c116635321f7dcdef72a9e58b2a56cab24cdf11
Tim Kilian <tim.kilian@edel-optics.de>
2019-08-09 17:35:41 +0200

    Add ignoreComments and ignoreRegExpLiterals

--- index.js
+++ index.js
@@ -9,35 +9,48 @@ module.exports = {
         'object-curly-spacing': [ 2, 'always' ],
         'array-bracket-spacing': [ 2, 'always' ],
         'computed-property-spacing': 'off',
-        'require-jsdoc': [ 0, {
-            'require': {
-                'FunctionDeclaration': true,
-                'MethodDefinition': true,
-                'ClassDeclaration': true
+        'require-jsdoc': [
+            0,
+            {
+                'require': {
+                    'FunctionDeclaration': true,
+                    'MethodDefinition': true,
+                    'ClassDeclaration': true
+                }
             }
-        } ],
+        ],
         'padded-blocks': [ 2, { classes: 'always' } ],
-        'vue/order-in-components': [ "warning", {
-            "order": [
-                "el",
-                "name",
-                "parent",
-                "functional",
-                ["delimiters", "comments"],
-                ["components", "directives", "filters"],
-                "extends",
-                "mixins",
-                "inheritAttrs",
-                "model",
-                ["props", "propsData"],
-                "data",
-                "computed",
-                "LIFECYCLE_HOOKS",
-                "methods",
-                "watch",
-                ["template", "render"],
-                "renderError",
-            ]
-        } ]
+        'vue/order-in-components': [
+            'warning',
+            {
+                'order': [
+                    'el',
+                    'name',
+                    'parent',
+                    'functional',
+                    [ 'delimiters', 'comments' ],
+                    [ 'components', 'directives', 'filters' ],
+                    'extends',
+                    'mixins',
+                    'inheritAttrs',
+                    'model',
+                    [ 'props', 'propsData' ],
+                    'data',
+                    'computed',
+                    'LIFECYCLE_HOOKS',
+                    'methods',
+                    'watch',
+                    [ 'template', 'render' ],
+                    'renderError',
+                ]
+            }
+        ],
+        'max-len': [
+            'error',
+            {
+                'ignoreComments': true,
+                'ignoreRegExpLiterals': true,
+            }
+        ],
     }
 };
